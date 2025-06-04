527354705e89d54fd0e6239592315956956a9111
Matheus Silva Santos <contact@mths.dev.br>
2025-06-03 21:43:10 -0300

    Reviewed MicrontekReceiver

--- projects/HCTManagerService/app/src/main/java/android/microntek/service/MicrontekReceiver.java
+++ projects/HCTManagerService/app/src/main/java/android/microntek/service/MicrontekReceiver.java
@@ -4,42 +4,35 @@ import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.microntek.Constant;
 import android.os.UserHandle;
 import android.util.Log;
-/* loaded from: classes.dex */
+
+/**
+ * MicrontekReceiver is a BroadcastReceiver that listens for system boot events
+ * and starts the ManageService when the device boots up.
+ */
 public class MicrontekReceiver extends BroadcastReceiver {
-    @Override // android.content.BroadcastReceiver
+
+    private static final String TAG = "MicrontekReceiver";
+
+    public static final ComponentName MANAGE_SERVICE_COMPONENT =
+            new ComponentName("android.microntek.service", "android.microntek.service.ManageService");
+
+    @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
-        if (action.equals("android.intent.action.BOOT_COMPLETED")) {
-            Log.i("chun", "MicrontekReceiver>>>>> BOOT_COMPLETED");
-            startMtcManage(context);
-            GpsTimeService(context);
-            RadioService(context);
-        } else if (action.equals("android.intent.action.LOCKED_BOOT_COMPLETED")) {
-            Log.i("chun", "MicrontekReceiver>>>>> LOCKED_BOOT_COMPLETED");
+        if (action.equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Log.i(TAG, "BOOT_COMPLETED");
+            startManageService(context);
+        } else if (action.equals(Intent.ACTION_LOCKED_BOOT_COMPLETED)) {
+            Log.i(TAG, "LOCKED_BOOT_COMPLETED");
         }
     }
 
-    private void startMtcManage(Context context) {
+    private void startManageService(Context context) {
         Intent intent = new Intent();
-        intent.setComponent(new ComponentName("android.microntek.service", "android.microntek.service.ManageService"));
-        context.startServiceAsUser(intent, UserHandle.OWNER);
+        intent.setComponent(MANAGE_SERVICE_COMPONENT);
+        context.startServiceAsUser(intent, UserHandle.SYSTEM);
     }
 
-    private void GpsTimeService(Context context) {
-        Intent gpstimeserviceintent = new Intent();
-        gpstimeserviceintent.setComponent(new ComponentName("android.microntek.service", "android.microntek.service.MicrontekTimeUpdateServer"));
-        context.startServiceAsUser(gpstimeserviceintent, UserHandle.OWNER);
-    }
-
-    private void RadioService(Context context) {
-        try {
-            Intent radioserviceintent = new Intent();
-            radioserviceintent.setComponent(new ComponentName(Constant.RADIOPACKAGE, "com.microntek.radio.RadioService"));
-            context.startServiceAsUser(radioserviceintent, UserHandle.OWNER);
-        } catch (Exception e) {
-        }
-    }
 }
