e34d060c19157bc5f7d23b09a89b31e41abf9de5
Matheus Silva Santos <contact@mths.dev.br>
2026-04-29 00:33:08 -0300

    MicrontekReceiver extending customized receiver

--- projects/HCTManagerService/app/src/main/java/android/microntek/service/MicrontekReceiver.java
+++ projects/HCTManagerService/app/src/main/java/android/microntek/service/MicrontekReceiver.java
@@ -1,38 +1,6 @@
 package android.microntek.service;
 
-import android.content.BroadcastReceiver;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.os.UserHandle;
-import android.util.Log;
+import android.mths.receivers.BootCompletedReceiver;
 
-/**
- * MicrontekReceiver is a BroadcastReceiver that listens for system boot events
- * and starts the ManageService when the device boots up.
- */
-public class MicrontekReceiver extends BroadcastReceiver {
-
-    private static final String TAG = "MicrontekReceiver";
-
-    public static final ComponentName MANAGE_SERVICE_COMPONENT =
-            new ComponentName("android.microntek.service", "android.microntek.service.ManageService");
-
-    @Override
-    public void onReceive(Context context, Intent intent) {
-        String action = intent.getAction();
-        if (action.equals(Intent.ACTION_BOOT_COMPLETED)) {
-            Log.i(TAG, "BOOT_COMPLETED");
-            startManageService(context);
-        } else if (action.equals(Intent.ACTION_LOCKED_BOOT_COMPLETED)) {
-            Log.i(TAG, "LOCKED_BOOT_COMPLETED");
-        }
-    }
-
-    private void startManageService(Context context) {
-        Intent intent = new Intent();
-        intent.setComponent(MANAGE_SERVICE_COMPONENT);
-        context.startServiceAsUser(intent, UserHandle.SYSTEM);
-    }
-
+public class MicrontekReceiver extends BootCompletedReceiver {
 }
