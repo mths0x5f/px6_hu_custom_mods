cda787279782f67550d48960e4d94d5359dc757f
Matheus Silva Santos <contact@mths.dev.br>
2026-06-21 23:16:27 -0300

    + BaseReceiver

---
+++ projects/CoreServices/app/src/main/java/br/dev/mths/coreservices/receivers/BaseReceiver.java
@@ -0,0 +1,52 @@
+package br.dev.mths.coreservices.receivers;
+
+import androidx.annotation.NonNull;
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.IntentFilter;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+@Getter(AccessLevel.PROTECTED)
+public abstract class BaseReceiver<L extends BaseReceiver.Listener> extends BroadcastReceiver {
+
+    private boolean isReceiverRegistered;
+    @NonNull
+    private final Context appContext;
+    @NonNull
+    private final L listener;
+    @NonNull
+    private final IntentFilter intentFilter;
+
+    protected interface Listener {
+    }
+
+    protected BaseReceiver(@NonNull Context context,
+                           @NonNull L listener,
+                           @NonNull IntentFilter intentFilter) {
+        this.appContext = context.getApplicationContext();
+        this.listener = listener;
+        this.intentFilter = intentFilter;
+    }
+
+    @SuppressLint("UnspecifiedRegisterReceiverFlag")
+    public void register() {
+        if (!isReceiverRegistered) {
+            appContext.registerReceiver(this, intentFilter);
+            isReceiverRegistered = true;
+        }
+    }
+
+    public void unregister() {
+        if (isReceiverRegistered) {
+            try {
+                appContext.unregisterReceiver(this);
+                isReceiverRegistered = false;
+            } catch (IllegalArgumentException ignored) {
+            }
+        }
+    }
+
+}
