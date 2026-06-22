4732ec3afa685fca6e1914ba28a1080f5d9da02d
Matheus Silva Santos <contact@mths.dev.br>
2026-06-21 23:12:35 -0300

    + LocaleChangeReceiver

---
+++ projects/CoreServices/app/src/main/java/br/dev/mths/coreservices/receivers/LocaleChangeReceiver.java
@@ -0,0 +1,33 @@
+package br.dev.mths.coreservices.receivers;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+public class LocaleChangeReceiver extends BaseReceiver<LocaleChangeReceiver.LocaleChangeListener> {
+
+    private static final IntentFilter INTENT_FILTER;
+
+    static {
+        INTENT_FILTER = new IntentFilter();
+        INTENT_FILTER.addAction(Intent.ACTION_LOCALE_CHANGED);
+    }
+
+    public LocaleChangeReceiver(Context context, LocaleChangeListener listener) {
+        super(context, listener, INTENT_FILTER);
+    }
+
+    public interface LocaleChangeListener extends Listener {
+        void onLocaleChanged();
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        LocaleChangeListener listener = getListener();
+        String action = intent.getAction();
+        if (action.equals(Intent.ACTION_LOCALE_CHANGED)) {
+            listener.onLocaleChanged();
+        }
+    }
+
+}
